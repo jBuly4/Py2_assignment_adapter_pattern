9950aca6c121a8d265b479625693dac8793e20bc
jBuly4 <ibuly4@gmail.com>
2021-06-17 11:40:28 +0300

    done + solution

--- adapter_solution.py
+++ adapter_solution.py
@@ -45,13 +45,42 @@ class MappingAdapter:
         #find where are lights and where are blocks
         obstacles = []
         lights = []
-        for row, lst in enumerate(grid):
-            for column, value in enumerate(lst):
+        for column, lst in enumerate(grid):
+            for row, value in enumerate(lst):
                 if value == 1:
-                    lights.append(row, column)
+                    lights.append((row, column))
                 elif value == -1:
-                    obstacles.append(row, column)
+                    obstacles.append((row, column))
         self.adaptee.set_lights(lights)
         self.adaptee.set_obstacles(obstacles)
         light_map = self.adaptee.generate_lights()
         return light_map
+
+'''
+# Реализуем адаптер для класса
+class MappingAdapter:
+
+    def __init__(self, adaptee):
+        # Сохраним адаптируемый объект
+        self.adaptee = adaptee
+
+    def lighten(self, grid):
+        # Определим метод рассчета освещенности
+        dim = (len(grid[0]), len(grid)) # Определение размера карты
+        self.adaptee.set_dim(dim) # Установка размера карты в адаптируемом объекте
+        # Инициализируем пустые списки препятствий и источников света
+        obst = []
+        lght = []
+        # Считаем положения объектов с исходной карты
+        for i in range(dim[0]):
+            for j in range(dim[1]):
+                if grid[j][i] == 1:
+                    lght.append((i,j))
+                elif grid[j][i] == -1:
+                    obst.append((i,j))
+        # Передадим положения объектов адаптируемому объекту
+        self.adaptee.set_lights(lght)
+        self.adaptee.set_obstacles(obst)
+        # Вернем полученную карту освещенности
+        return self.adaptee.grid
+'''
